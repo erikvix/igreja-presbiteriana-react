a8f3a46b3e84a63c2cc0e1a8e20f973685c9259f
Erik Nunes <erikvixx@gmail.com>
2023-12-05 10:59:23 -0300

    feat: include event banner

--- src/components/Eventos/index.jsx
+++ src/components/Eventos/index.jsx
@@ -3,29 +3,31 @@ import Button from "../Button";
 
 export default function Eventos() {
   return (
-    <div className="flex flex-col ">
-      <div className="flex items-center gap-4 py-8">
-        <h1 className="font-bold uppercase text-4xl text-dark-green">
-          Próximos eventos
+    <div className="flex flex-col mt-10">
+      <div className="flex items-center gap-6 py-8">
+        <h1 className="font-bold uppercase text-3xl text-dark-green px-0">
+          Próximos Eventos
         </h1>
-        <div className=" border-b-8 border-dark-red w-96 rounded"></div>
+        <div className=" border-b-8 border-dark-red rounded w-96"></div>
       </div>
       <div className="bg-light-gray flex shadow-xl">
         <div>
           <img src="https://placehold.co/200x200" alt="" />
         </div>
-        <div className="flex items-center justify-between  w-full">
-          <div className="flex flex-col items-center px-6">
-            <h1 className="text-dark-green font-bold uppercase">11</h1>
-            <h1 className="text-dark-green font-bold uppercase">Jun</h1>
+        <div className="flex items-center w-full basis-1/1">
+          <div className="flex flex-col items-center px-8">
+            <h1 className="text-2xl text-dark-green font-bold uppercase">11</h1>
+            <h1 className="text-2xl text-dark-green font-bold uppercase">
+              Jun
+            </h1>
           </div>
-          <div>
-            <h1 className="text-dark-green font-bold uppercase">
+          <div className="flex flex-col basis-2/3">
+            <h1 className="text-2xl text-dark-green font-bold uppercase">
               fechados para reforma
             </h1>
-            <h3 className="text-dark-green">horarios</h3>
+            <h3 className="text-dark-green font-medium">12:00 - 20:00</h3>
           </div>
-          <div className="px-6">
+          <div className="basis-1/4 flex justify-center">
             <Button>Inscrever-se</Button>
           </div>
         </div>
