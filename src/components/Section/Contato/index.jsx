f38dd70d470f981a9028659df6c8c159cd9e3f7d
Erik <erikvixx@gmail.com>
2023-12-06 16:23:58 -0300

    feat: include contact section

--- src/components/Section/Contato/index.jsx
+++ src/components/Section/Contato/index.jsx
@@ -3,26 +3,30 @@ import Button from "../../Button";
 
 export default function Contato() {
   return (
-    <div className="flex justify-center flex-col items-center">
+    <div className="flex justify-center flex-col items-center bg-neutral-200 py-10">
       <div className="flex flex-col items-center gap-6 mb-8">
-        <h1>Contato</h1>
-        <div className="border-b-4 border-red-500 rounded w-96"></div>
+        <h1 className="font-bold uppercase text-3xl text-dark-green">
+          Contato
+        </h1>
+        <div className="border-b-4 border-dark-red rounded w-[1000px]"></div>
       </div>
-      <div className="flex flex-col w-96">
-        <label htmlFor="">d</label>
+      <div className="flex flex-col">
+        <label htmlFor="">Assunto:</label>
         <input type="text" />
-        <label htmlFor="">d</label>
+        <label htmlFor="">E-mail:</label>
         <input type="text" />
-        <label htmlFor="">d</label>
+        <label htmlFor="">Telefone (Opcional):</label>
         <input type="text" />
-        <label htmlFor="">d</label>
-        <input type="text" />
+        <label htmlFor="">Mensagem:</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
         <div className="flex justify-between mt-6">
-          <span>t</span>
-          <Button />
+          <span className="text-xs text-black-c">
+            Encaminhado para o e-mail pastor.romulo@primeiraipcg.exemplo.com
+          </span>
+          <Button>Enviar</Button>
         </div>
       </div>
-      <span>whatsaapp</span>
+      <a href="">Clique para falar com +55 27 99514-2114 no WhatsApp</a>
     </div>
   );
 }
--- src/components/Section/LocalEHorarios/index.jsx
+++ src/components/Section/LocalEHorarios/index.jsx
@@ -5,23 +5,17 @@ const items = [
   {
     image: "https://placehold.co/190",
     date: "18:00 - 20:15",
-    title: "FECHADOS PARA REFORMA",
-    day: "11",
-    month: "jun",
+    title: "Seg à sexta",
   },
   {
     image: "https://placehold.co/190",
-    date: "20:00 - 21:30",
-    title: "LIBERDADE",
-    day: "3",
-    month: "jul",
+    date: "10:30 - 20:00",
+    title: "Domingo",
   },
   {
     image: "https://placehold.co/190",
-    date: "20:00 - 21:00",
-    title: "ESPERANÇA",
-    day: "8",
-    month: "out",
+    date: "18:00 - 21:30",
+    title: "Escola biblíca",
   },
 ];
 
@@ -30,7 +24,7 @@ export default function LocalEHorarios() {
     <div className="bg-dark-green">
       <div className="flex items-baseline justify-between py-8 px-8">
         <div className="flex flex-col gap-6">
-          <h1>Nosso Podcast</h1>
+          <h1 className="font-bold uppercase ">Nosso Podcast</h1>
           {/* <video
             src="https://www.youtube.com/watch?v=NpEaa2P7qZI"
             autoPlay
@@ -40,7 +34,7 @@ export default function LocalEHorarios() {
           <img src="https://placehold.co/600x350" alt="" />
         </div>
         <div className="max-w-2xl flex flex-col gap-6">
-          <h1>Local e horários</h1>
+          <h1 className="font-bold uppercase">Local e horários</h1>
           {items.map((item) => {
             return (
               <EventoBanner
