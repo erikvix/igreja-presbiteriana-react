131d1ff8b18c89f596034965faa607172f355e0d
Erik Nunes <erikvixx@gmail.com>
2023-12-11 16:47:30 -0300

    refactor: fix img width for card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Card({ title, desc, src, pathName = "#" }) {
   return (
-    <div class=" max-w-sm p-6 bg-white rounded-lg shadow-lg dark:bg-light-gray">
-      <img class="h-auto max-w-full rounded-lg" src={src} alt="" />
+    <div class=" max-w-sm p-6 bg-wh ite rounded-lg shadow-lg dark:bg-light-gray">
+      <img
+        class="h-[300px] w-[500px] object-cover rounded-lg"
+        src={src}
+        alt=""
+      />
       <Link to={pathName}>
-        <h5 class="object-cover my-2 text-2xl font-bold tracking-tight text-black-c">
+        <h5 class="object-cover capitalize my-2 text-2xl font-bold tracking-tight text-black-c">
           {title}
         </h5>
       </Link>
